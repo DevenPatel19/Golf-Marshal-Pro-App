52e33c9bb71c04e240c70776dc913685e8663837
Deven <deven.h.j.patel+python@gmail.com>
2025-01-28 12:50:38 -0800

    Created react app and installed web-vitals

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const liveTitle = "Sanity Test"
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>{liveTitle}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
